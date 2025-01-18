3726fccbc6e8bf419a14a5b83035923ef6d3349a
DjamelAyari <ayari240895@hotmail.fr>
2025-01-18 15:03:22 +0100

    Modification 18/01/2025 15:03

--- server/process_request.c
+++ server/process_request.c
@@ -99,12 +99,26 @@ void send_file(SSL *ssl, char *file_path)
     rewind(ptr_file);
 
     char response_header[256];
+    if(strstr(path_parse, "html"))
+    {
+    //char response_header[256];
     snprintf(response_header, sizeof(response_header),
     "HTTP/1.1 200 OK\r\n"
     "Content-Type: text/html; charset=utf-8\r\n"
     "Content-Length: %ld\r\n"
     "Connection: close\r\n\r\n",
     file_size);
+    }
+    else if(strstr(path_parse, "css"))
+    {
+        //char response_header[256];
+    snprintf(response_header, sizeof(response_header),
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: text/css; charset=utf-8\r\n"
+    "Content-Length: %ld\r\n"
+    "Connection: close\r\n\r\n",
+    file_size);
+    }
 
     SSL_write(ssl, response_header, strlen(response_header));
 
